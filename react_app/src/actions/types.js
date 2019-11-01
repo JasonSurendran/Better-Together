dfb236dba9fab8e4c6951f5bf4b4f7968eb78a9e
JasonSurendran <surendranjason@gmail.com>
2019-11-01 09:53:25 -0400

    Alert Reducer

---
+++ react_app/src/actions/types.js
@@ -0,0 +1,4 @@
+//Compiled list of all action type constants
+//Allows any changes to occur from one centralized file
+export const SET_ALERT = 'SET_ALERT';
+export const REMOVE_ALERT = 'REMOVE_ALERT';
