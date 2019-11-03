239bc1eda4f13b1c25dc64f1f7aeced5705246ca
JasonSurendran <surendranjason@gmail.com>
2019-11-03 09:35:59 -0500

    Set timeout for alerts

--- react_app/src/actions/alert.js
+++ react_app/src/actions/alert.js
@@ -1,10 +1,15 @@
 import uuid from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
-export const setAlert = () => dispatch => {
+//Display type of alert and add it to state in reducers/alert.js
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
+    //Generate ID for that alert
     const id = uuid.v4();
+    //dispatch alert
     dispatch({
         type: SET_ALERT,
         payload: { msg, alertType, id }
     });
+
+    setTimeout(()=> dispatch({ type: REMOVE_ALERT, payload: id}), timeout);
 }
