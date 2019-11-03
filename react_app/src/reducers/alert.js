4ca571355d34244a32ee9f38380b84ffe3f829f6
JasonSurendran <surendranjason@gmail.com>
2019-11-03 09:36:14 -0500

    Add comments

--- react_app/src/reducers/alert.js
+++ react_app/src/reducers/alert.js
@@ -1,10 +1,12 @@
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types'
 const initialState = [];
 
+//Add alert to state
 export default function(state = initialState, action){
     const { type, payload } = action;
     switch(type){
         case SET_ALERT:
+            //Add alert to array (see with redux dev tools)
             return [...state, payload];
         case REMOVE_ALERT:
             return state.filter(alert => alert.id !== payload);
