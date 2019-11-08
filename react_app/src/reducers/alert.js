66b95764c8a2a2064283cfcd9fd826f113d20788
JasonSurendran <surendranjason@gmail.com>
2019-11-07 19:51:17 -0500

    Fix formatting

--- react_app/src/reducers/alert.js
+++ react_app/src/reducers/alert.js
@@ -1,6 +1,12 @@
+//Reducer Definition
+/*  A reducer is a function that accepts an accumulation and a value and returns a new accumulation. 
+    They are used to reduce a collection of values down to a single value.
+    In Redux, the accumulated value is the state object, and the values being accumulated are actions. 
+    Reducers calculate a new state given the previous state and an action. 
+    As your app grows more complex, you'll want to split your reducing function into separate functions, each managing independent parts of the state.*/
+
 //Imports
 import { SET_ALERT, REMOVE_ALERT } from '../actions/types'
-
 
 const initialState = [];
 
--- react_app/src/store.js
+++ react_app/src/store.js
@@ -1,10 +1,3 @@
-//Reducer Definition
-/*  A reducer is a function that accepts an accumulation and a value and returns a new accumulation. 
-    They are used to reduce a collection of values down to a single value.
-    In Redux, the accumulated value is the state object, and the values being accumulated are actions. 
-    Reducers calculate a new state given the previous state and an action. 
-    As your app grows more complex, you'll want to split your reducing function into separate functions, each managing independent parts of the state.*/
-
 //Imports
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
